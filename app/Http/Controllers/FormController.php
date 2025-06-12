3cd81114d9f1d14956b2213382d2130ba3eff473
Assane <adiop6225@gmail.com>
2025-06-12 10:29:25 +0200

    message

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -4,8 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactMessage;
 use Illuminate\Support\Facades\Http;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+
 use App\Models\ContactMessageLog;
 
 
@@ -41,10 +40,10 @@ public function send(Request $request)
         'emails.confirmation_client'
     ));
 
-    $this->sendTelegramNotifications($data, 'classic');
+   
 
     
-     return response()->json(['status' => 'ok']);
+    return response()->json(['status' => 'ok']);
 }
 
 public function Contact(Request $request)
@@ -76,39 +75,13 @@ public function Contact(Request $request)
         'emails.confirmation_client'
     ));
 
-    $this->sendTelegramNotifications($data, 'page');
+   
 
-    //  return response()->json(['status' => 'ok']);
-     return redirect()->back()->with('success', 'Votre message a bien été envoyé.');
+     return response()->json(['status' => 'ok']);
+   
 }
 
 
-private function sendTelegramNotifications($data, $type = 'classic')
-{
-    $token = env('TELEGRAM_BOT_TOKEN');
-    $chatId = env('TELEGRAM_CHAT_ID');
-
-    if ($type === 'classic') {
-        $message = "📩 <b>Nouveau message classique</b>\n"
-            . "<b>Nom :</b> {$data['name']}\n"
-            . "<b>Email :</b> {$data['email']}\n"
-            . "<b>Téléphone :</b> {$data['phone']}\n"
-            . "<b>Projet :</b> {$data['projet']}\n"
-            . "<b>Message :</b>\n{$data['message']}";
-    } else {
-        $message = "📬 <b>Nouveau message page Contact</b>\n"
-            . "<b>Nom :</b> {$data['fname']} {$data['lname']}\n"
-            . "<b>Email :</b> {$data['email']}\n"
-            . "<b>Téléphone :</b> {$data['phone']}\n"
-            . "<b>Message :</b>\n{$data['message']}";
-    }
-
-    Http::post("https://api.telegram.org/bot{$token}/sendMessage", [
-        'chat_id' => $chatId,
-        'text' => $message,
-        'parse_mode' => 'HTML',
-    ]);
-}
 
 
 }
